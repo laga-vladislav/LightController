4239724a64c93218507c464cfb46f2ef27fe1c35
laga-vladislav <laga.ladislav@yandex.ru>
2023-07-21 20:49:05 +0900

    Modified WizLightController: Added get_status method and refactored the code using access modifiers

--- LightController/WizBulbController.py
+++ LightController/WizBulbController.py
@@ -1,33 +1,36 @@
-from enum import Enum
+import pywizlight.bulb
 from pywizlight import wizlight, PilotBuilder
 
-class Status(Enum):
-    Off = 0
-    On = 1
-
-
 class WizBulbController:
-    def __init__(self, wizlight_bulb: wizlight) -> None:
-        self.bulb = wizlight_bulb
-        self.pilot_builder = PilotBuilder
-        self.status = Status[self.bulb.state]
+    def __init__(
+            self,
+            wizlight_bulb: wizlight,
+            pilot_builder: PilotBuilder = PilotBuilder()
+    ) -> None:
+        self._bulb: wizlight = wizlight_bulb
+        self._pilot_builder: PilotBuilder = pilot_builder
+        self._config: pywizlight.bulb.PilotParser
+        self._status: bool
 
     def __dict__(self):
         return {
-            'ip': self.bulb.ip,
-            'mac': self.bulb.mac
+            'ip': self._bulb.ip,
+            'mac': self._bulb.mac
         }
 
-    async def turn_on(
-            self,
-            pilot_builder: PilotBuilder = PilotBuilder(colortemp=4400, brightness=255)
-    ):
-        await self.bulb.turn_on(pilot_builder=pilot_builder)
-        self.status = Status.On
-
-    async def turn_off(self):
-        await self.bulb.turn_off()
-        self.status = Status.Off
+    async def __update_config(self):
+        self._config = await self._bulb.updateState()
 
     async def get_status(self):
-        print(self.bulb.status)
+        await self.__update_status()
+        return self._status
+
+    async def __update_status(self):
+        await self.__update_config()
+        self._status = self._config.get_state()
+
+    async def turn_on(self):
+        await self._bulb.turn_on(pilot_builder=self._pilot_builder)
+
+    async def turn_off(self):
+        await self._bulb.turn_off()
