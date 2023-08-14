cb3b38082c47c543f97229dca41394e758dd78d3
laga-vladislav <laga.ladislav@yandex.ru>
2023-08-14 15:39:24 +0900

    Added new exceptions for WizBulbProfile

--- LightController/exceptions/_exceptions.py
+++ LightController/exceptions/_exceptions.py
@@ -1,3 +1,25 @@
 class RetriesError(Exception):
     """Too many tries"""
     pass
+
+class BrightnessValueError(Exception):
+    """Wrong brightness value"""
+    pass
+
+class BrightnessTypeError(Exception):
+    """Wrong type of brightness value"""
+    pass
+
+class LightTemperatureValueError(Exception):
+    """Wrong kelvin value"""
+    pass
+
+class LightTemperatureTypeError(Exception):
+    """Wrong type of kelvin value"""
+    pass
+class RGBValueError(Exception):
+    """Wrong RGB value"""
+    pass
+class RGBTypeError(Exception):
+    """Wrong type of RGB value"""
+    pass
