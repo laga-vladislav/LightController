bcef0f5450078a7037ee473e0c760eddf298833e
laga-vladislav <laga.ladislav@yandex.ru>
2023-08-17 23:15:43 +0900

    Added config file with the application settings

---
+++ LightController/Core/config.py
@@ -0,0 +1,12 @@
+from typing import Union
+from pywizlight import wizlight
+from .library import Aliases
+
+TIME_FORMAT_FOR_LOGGER = '%X'
+
+INSTANCE_BULB_TYPES = Union[wizlight]  # add new types later
+MIN_BRIGHTNESS_VALUE: Aliases.HexCode = 0
+MAX_BRIGHTNESS_VALUE: Aliases.HexCode = 255
+
+LOCAL_IP = "192.168.1.255"
+MAX_ATTEMPTS_TO_FIND_BULBS = 3
